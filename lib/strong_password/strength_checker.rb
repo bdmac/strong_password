82e528ec7d90fed1333b571e9df429cc854cd409
Jacob Atzen <jatzen@gmail.com>
2019-02-01 09:38:42 +0100

    Swap StrengthChecker initalizer args and method args

--- lib/strong_password/strength_checker.rb
+++ lib/strong_password/strength_checker.rb
@@ -4,20 +4,21 @@ module StrongPassword
     PASSWORD_LIMIT = 1_000
     EXTRA_WORDS_LIMIT = 1_000
 
-    attr_reader :base_password
+    attr_reader :min_entropy, :use_dictionary, :min_word_length, :extra_dictionary_words
 
-    def initialize(password)
-      @base_password = password.dup[0...PASSWORD_LIMIT]
+    def initialize(min_entropy: BASE_ENTROPY, use_dictionary: false, min_word_length: 4, extra_dictionary_words: [])
+      @min_entropy = min_entropy
+      @use_dictionary = use_dictionary
+      @min_word_length = min_word_length
+      @extra_dictionary_words = extra_dictionary_words
     end
 
-    def is_weak?(min_entropy: BASE_ENTROPY, use_dictionary: false, min_word_length: 4, extra_dictionary_words: [])
-      !is_strong?(min_entropy: min_entropy,
-                  use_dictionary: use_dictionary, 
-                  min_word_length: min_word_length, 
-                  extra_dictionary_words: extra_dictionary_words)
+    def is_weak?(password)
+      !is_strong?(password)
     end
 
-    def is_strong?(min_entropy: BASE_ENTROPY, use_dictionary: false, min_word_length: 4, extra_dictionary_words: [])
+    def is_strong?(password)
+      base_password = password.dup[0...PASSWORD_LIMIT]
       weak = (EntropyCalculator.calculate(base_password) < min_entropy) ||
              (EntropyCalculator.calculate(base_password.downcase) < min_entropy) ||
              (QwertyAdjuster.new(min_entropy: min_entropy).is_weak?(base_password))
@@ -32,7 +33,8 @@ module StrongPassword
       end
     end
 
-    def calculate_entropy(use_dictionary: false, min_word_length: 4, extra_dictionary_words: [])
+    def calculate_entropy(password)
+      base_password = password.dup[0...PASSWORD_LIMIT]
       extra_dictionary_words.collect! { |w| w[0...EXTRA_WORDS_LIMIT] }
       entropies = [EntropyCalculator.calculate(base_password), EntropyCalculator.calculate(base_password.downcase), QwertyAdjuster.new.adjusted_entropy(base_password)]
       entropies << DictionaryAdjuster.new(
--- spec/strong_password/strength_checker_spec.rb
+++ spec/strong_password/strength_checker_spec.rb
@@ -11,7 +11,7 @@ module StrongPassword
         'aB$1' => false
       }.each do |password, strength|
         it "is_strong? returns #{strength} for '#{password}' with 12 bits of entropy" do
-          expect(StrengthChecker.new(password).is_strong?(min_entropy: 12)).to be(strength)
+          expect(StrengthChecker.new(min_entropy: 12).is_strong?(password)).to be(strength)
         end
       end
     end
@@ -26,7 +26,7 @@ module StrongPassword
         'aB$1' => false
       }.each do |password, strength|
         it "is_strong? returns #{strength} for '#{password}' with 12 bits of entropy" do
-          expect(StrengthChecker.new(password).is_strong?(min_entropy: 12, use_dictionary: true)).to eq(strength)
+          expect(StrengthChecker.new(min_entropy: 12, use_dictionary: true).is_strong?(password)).to eq(strength)
         end
       end
     end
@@ -41,7 +41,7 @@ module StrongPassword
         'correct horse battery staple' => true
       }.each do |password, strength|
         it "is_strong? returns #{strength} for '#{password}' with standard bits of entropy" do
-          expect(StrengthChecker.new(password).is_strong?(use_dictionary: true)).to eq(strength)
+          expect(StrengthChecker.new(use_dictionary: true).is_strong?(password)).to eq(strength)
         end
       end
     end
@@ -57,20 +57,22 @@ module StrongPassword
         'c0rr#ct h0rs3 Batt$ry st@pl3 is Gr34t' => true
       }.each do |password, strength|
         it "is_strong? returns #{strength} for '#{password}' with standard bits of entropy" do
-          expect(StrengthChecker.new(password).is_strong?(min_entropy: 40, use_dictionary: true)).to eq(strength)
+          expect(StrengthChecker.new(min_entropy: 40, use_dictionary: true).is_strong?(password)).to eq(strength)
         end
       end
     end
 
     context 'with long password' do
-      let(:strength_checker) { StrengthChecker.new("ba"*500_000) }
+      let(:strength_checker) { StrengthChecker.new }
+      let(:password) { ("ba"*500_000) }
       it 'should be truncated' do
-        expect(strength_checker.instance_variable_get(:@base_password).length).to eq StrengthChecker::PASSWORD_LIMIT
+        expect(strength_checker.calculate_entropy(password)).
+          to eq(strength_checker.calculate_entropy(password.slice(0, StrengthChecker::PASSWORD_LIMIT)))
       end
     end
 
     context 'with long extra words' do
-      let(:strength_checker) { StrengthChecker.new("$tr0NgP4s$w0rd91d£") }
+      let(:strength_checker) { StrengthChecker.new(use_dictionary: true, extra_dictionary_words: ["a"*1_000_000, "b"*10_000_000, "c"*10]) }
       let(:exta_limit) { StrengthChecker::EXTRA_WORDS_LIMIT }
       it 'should be truncated' do
         expect(DictionaryAdjuster).to receive(:new).with({
@@ -78,7 +80,7 @@ module StrongPassword
             extra_dictionary_words:
             ["a"*StrengthChecker::EXTRA_WORDS_LIMIT, "b"*StrengthChecker::EXTRA_WORDS_LIMIT, "c"*10]
           }).and_call_original
-        strength_checker.calculate_entropy(use_dictionary: true, extra_dictionary_words: ["a"*1_000_000, "b"*10_000_000, "c"*10])
+        strength_checker.calculate_entropy("$tr0NgP4s$w0rd91d£")
       end
     end
   end
